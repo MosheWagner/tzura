a46f99e84d6b6264a5a39ad26d2a171fe0bd5b27
moshe.wagner@gmail.com <moshe.wagner@gmail.com@fe19fda5-45ab-82a9-1c32-40079ea4bbeb>
2011-05-13 11:28:53 +0000

    Center line cheat

--- ui_help.h
+++ ui_help.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'help.ui'
 **
-** Created: Fri May 13 13:42:34 2011
+** Created: Fri May 13 13:49:32 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
